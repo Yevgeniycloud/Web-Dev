7383f4febc27531ffe267b852968152953fbc262
Yevgeniycloud <zazak1231@gmail.com>
2023-02-13 15:51:02 +0600

    jsinfo

---
+++ lab3/Javascript info/1.js
@@ -0,0 +1,15 @@
+console.log('hello world!');
+
+let admin='';
+let name = 'John';
+
+admin = name;
+console.log(admin);
+
+let namel = "Ilya";
+
+console.log( `hello ${1}` ); // ?
+
+console.log( `hello ${"namel"}` ); // ?
+
+console.log( `hello ${namel}` ); // ?
